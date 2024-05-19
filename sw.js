77bd7cc818ed280cb9a80a62e36d9e6b47542c14
Kniffen <kniffen@knifftech.org>
2024-05-19 17:53:18 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,30 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('message', event => {
+  console.log('Service worker received message ' + JSON.stringify(event))
+})
+
+self.addEventListener('sync', event => {
+  console.log('Service Worker syncing.');
+})
+
+self.addEventListener('push', event => {
+  if (event.data) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: 'icon.png',
+        badge: 'badge.png'
+    };
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
+});
